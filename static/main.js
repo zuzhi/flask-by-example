b41c36ea57a5a11bfc8bf61bbd621c353386f00b
zuzhi <476513451@qq.com>
2017-01-30 03:18:21 +0800

    update the front-end and added a chart using d3

--- static/main.js
+++ static/main.js
@@ -7,6 +7,10 @@
   .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
     function($scope, $log, $http, $timeout) {
 
+      $scope.submitButtonText = 'Submit';
+      $scope.loading = false;
+      $scope.urlerror = false;
+
       $scope.getResults = function() {
 
         $log.log("test");
@@ -19,7 +23,10 @@
           success(function(results) {
             $log.log(results);
             getWordCount(results);
-
+            $scope.wordcounts = null;
+            $scope.loading = true;
+            $scope.submitButtonText = 'Loading...';
+            $scope.urlerror = false;
           }).
           error(function(error) {
             $log.log(error);
@@ -39,6 +46,9 @@
               $log.log(data, status);
             } else if (status === 200){
               $log.log(data);
+              $scope.loading = false;
+              $scope.submitButtonText = "Submit";
+              $scope.urlerror = false;
               $scope.wordcounts = data;
               $timeout.cancel(timeout);
               return false;
@@ -46,11 +56,46 @@
             // continue to call the poller() function every 2 seconds
             // until the timeout is cancelled
             timeout = $timeout(poller, 2000);
+          }).
+          error(function(error) {
+            $log.log(error);
+            $scope.loading = false;
+            $scope.submitButtonText = "Submit";
+            $scope.urlerror = true;
           });
         };
         poller();
       }
 
+    }
+  ])
+
+  .directive('wordCountChart', ['$parse', function ($parse) {
+    return {
+      restrict: 'E',
+      replace: true,
+      template: '<div id="chart"></div>',
+      link: function (scope) {
+        scope.$watch('wordcounts', function() {
+          d3.select('#chart').selectAll('*').remove();
+          var data = scope.wordcounts;
+          for (var word in data) {
+            d3.select('#chart')
+              .append('div')
+              .selectAll('div')
+              .data(word[0])
+              .enter()
+              .append('div')
+              .style('width', function() {
+                return (data[word] * 20) + 'px';
+              })
+              .text(function(d){
+                return word;
+              });
+          }
+        }, true);
+      }
+     };
     }
   ]);
 
